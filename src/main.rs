1ade759d0f789d75fbe549f6703807c7f188c6f1
Jon Haddad <jon@jonhaddad.com>
2015-10-10 11:04:10 -0700

    properly connecting to the db now

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,34 @@ use capnp_rpc::ez_rpc::EzRpcServer;
 use cassandra::*;
 
 // this is generated automatically as part of the build process
-// see build.rs 
+// see build.rs
 pub mod killranswers_capnp {
   include!(concat!(env!("OUT_DIR"), "/killranswers_capnp.rs"));
 }
 use killranswers_capnp::killr_answers;
 
+// queries for prepared statements
+// schema is managed in the python part of the app
+// see the cqlengine Models
 static CREATE_USER : &'static str = "insert into user (user_id), values (?)";
 
-lazy_static! {
 
+struct KillrAnswersImpl {
+    db: CassSession,
 }
 
+impl KillrAnswersImpl {
+    // returns a Boxed implemention
+    fn new() -> Box<KillrAnswersImpl> {
 
-struct KillrAnswersImpl;
+        let mut cluster = CassCluster::new();
+        cluster.set_contact_points("127.0.0.1").unwrap();
+        cluster.set_protocol_version(3).unwrap();
+        let mut session = CassSession::new().connect(&mut cluster).wait().unwrap();
+
+        Box::new(KillrAnswersImpl{ db: session })
+    }
+}
 
 impl killr_answers::Server for KillrAnswersImpl {
     // fn ask(&mut self, mut context: killr_answers::AskContext) {
@@ -37,8 +51,9 @@ impl killr_answers::Server for KillrAnswersImpl {
 
 fn main() {
     println!("Starting up...");
+
     let rpc_server = EzRpcServer::new("127.0.0.1:6000").unwrap();
-    let ka = Box::new(killr_answers::ServerDispatch { server : Box::new(KillrAnswersImpl)});
+    let ka = Box::new(killr_answers::ServerDispatch { server : KillrAnswersImpl::new() });
     //
     rpc_server.serve(ka);
 
