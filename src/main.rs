5ca24bd7cd75a9d7d7084beccd66fa28550d0f2c
Jon Haddad <jon@jonhaddad.com>
2015-10-09 18:38:32 -0700

    so close to getting rust server running

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+extern crate capnp;
+extern crate capnp_rpc;
+// extern crate killranswers_capnp;
+
+use capnp::capability::{Server};
+use capnp_rpc::ez_rpc::EzRpcServer;
+// use killranswers_capnp::killranswers;
+
 
 fn main() {
+    println!("Starting up...");
+    let rpc_server = EzRpcServer::new("6000").unwrap();
+    // let killranswers = Box::new(calculator::ServerDispatch { server : Box::new(CalculatorImpl)});
+    //
+    // rpc_server.serve(killranswers);
+
     println!("Done");
 }
